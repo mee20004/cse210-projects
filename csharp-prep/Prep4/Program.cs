e698b19947b74377332db53518fbe9381524df33
mee20004 <mee20004@byui.edu>
2026-01-15 22:36:49 -0700

    Prep 3 - 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int UserNum = -1;
+        while (UserNum != 0)
+        {
+            Console.WriteLine("Enter a number (0 to quit): ");
+
+            string response = Console.ReadLine();
+            UserNum = int.Parse(response);
+
+            if (UserNum != 0)
+            {
+                numbers.Add(UserNum);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+        float avg = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {avg}");
+
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The Max is: {max}");
     }
 }
